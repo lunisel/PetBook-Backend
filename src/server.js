7b236dc98e708bff18ddd6697c95b7b39e341858
Luna Sarmiento <selene942008@hotmail.com>
2021-10-21 09:41:47 +0200

    started server and connected to db

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+
+const port = process.env.PORT | 3005
+const mongoConnection = process.env.MONGO_URL
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+
+mongoose.connect(mongoConnection, { useNewUrlParser: true }).then(() => {
+    server.listen(port, () => {
+      console.log("Server listening on port", port, "and connected to DB");
+    });
+  });
+
