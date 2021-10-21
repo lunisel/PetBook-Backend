552a2c1980d1db4d18b6d8b9806775c13f9cc457
Luna Sarmiento <selene942008@hotmail.com>
2021-10-21 09:43:52 +0200

    endpoint table

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
+import listEndpoints from "express-list-endpoints";
 
 const port = process.env.PORT | 3005
 const mongoConnection = process.env.MONGO_URL
@@ -8,6 +9,8 @@ const server = express()
 
 server.use(cors())
 server.use(express.json())
+
+console.table(listEndpoints(server))
 
 mongoose.connect(mongoConnection, { useNewUrlParser: true }).then(() => {
     server.listen(port, () => {
