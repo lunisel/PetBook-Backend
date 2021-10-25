04931b7ff6ab59177d2ce614d97f69e1c7196e37
Luna Sarmiento <selene942008@hotmail.com>
2021-10-25 17:32:50 +0200

    register route

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,12 +1,24 @@
 import express from "express";
 import UserModel from "./schema.js";
 import createHttpError from "http-errors";
+import { generateTokens } from "../../utils/jwt.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", async (req, resp, next) => {
   try {
     const newUser = new UserModel(req.body);
+    const users = await UserModel.find();
+    if (users.findIndex((u) => u.email === newUser.email) === -1) {
+      const { _id } = await newUser.save();
+      console.log("NEW USER SAVED🙌");
+      if (newUser) {
+        const { accessToken, refreshToken } = await generateTokens(newUser);
+        resp.status(201).send({ newUser, accessToken, refreshToken });
+      }
+    } else {
+      resp.status(422).send({ error: "Duplicate emails cannot be processed" });
+    }
   } catch (err) {
     next(err);
   }
--- src/services/users/schema.js
+++ src/services/users/schema.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const FriendSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
@@ -13,7 +14,7 @@ const FriendSchema = new mongoose.Schema({
 });
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  petName: { type: String, required: true },
   nickname: { type: String, required: false },
   username: { type: String, unique: true, required: true },
   avatar: { type: String, required: false },
@@ -23,16 +24,42 @@ const UserSchema = new mongoose.Schema({
   bio: { type: String, required: false },
   birthday: { type: String, required: false },
   city: { type: String, required: false },
-  myOwner: [
-    {
-      name: { type: String, required: false },
-      surname: { type: String, required: false },
-      avatar: { type: String, required: false },
-      birthday: { type: String, required: false },
-    },
-  ],
+  myOwner: {
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    avatar: { type: String, required: false },
+    birthday: { type: String, required: false },
+  },
   friends: [FriendSchema],
   refreshToken: { type: String, required: false },
 });
+
+UserSchema.pre("save", async function (next) {
+  const newUser = this;
+  const plainPW = newUser.password;
+  if (newUser.isModified("password")) {
+    newUser.password = await bcrypt.hash(plainPW, 11);
+    next();
+  }
+});
+
+UserSchema.methods.toJSON = function () {
+  const userDoc = this;
+  const userObj = userDoc.toObject();
+  delete userObj.password;
+  delete userObj.__v;
+  return userObj;
+};
+
+UserSchema.statics.checkCredentials = async function (email, plainPW) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const isMatch = await bcrypt.compare(plainPW, user.password);
+    if (isMatch) return user;
+    else return null;
+  } else {
+    return null;
+  }
+};
 
 export default mongoose.model("User", UserSchema);
