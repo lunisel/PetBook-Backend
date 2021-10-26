d263ae3a4647d026e39d19c901a18b833881c29f
Luna Sarmiento <selene942008@hotmail.com>
2021-10-26 11:01:46 +0200

    log in and get me with token

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import UserModel from "./schema.js";
 import createHttpError from "http-errors";
 import { generateTokens } from "../../utils/jwt.js";
+import {JWTAuthMiddleware} from "../../utils/middlewares.js"
 
 const userRouter = express.Router();
 
+
+//REGISTRATION
 userRouter.post("/", async (req, resp, next) => {
   try {
     const newUser = new UserModel(req.body);
@@ -21,6 +24,33 @@ userRouter.post("/", async (req, resp, next) => {
     }
   } catch (err) {
     next(err);
+  }
+});
+
+//LOGIN
+userRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.checkCredentials(email, password);
+    if (user !== null) {
+      const { accessToken, refreshToken } = await generateTokens(user);
+      res.send({ accessToken, refreshToken });
+      console.log("🔸USER LOGGED IN BY EMAIL, PASSWORD🙌");
+    } else {
+      res.status(401).send("👻 Something's wrong with your credentials!");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+//GET ME FROM TOKEN
+userRouter.get("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    res.send(req.user);
+    console.log("🔸USER FETCHED BY TOKEN🙌");
+  } catch (error) {
+    next(error);
   }
 });
 
